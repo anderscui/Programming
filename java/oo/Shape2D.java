621e950f7bf424ed0820fd9a5c6518962cca499c
andersc <anderscui@gmail.com>
2016-01-23 01:22:43 +0800

    Inheritance in Java.

--- java/oo/Shape2D.java
+++ java/oo/Shape2D.java
@@ -1,28 +1,76 @@
-public class Shape2D {
+public abstract class Shape2D {
 
-    double width;
-    double height;
+    private double width;
+    private double height;
+    private String kind;
+
+    Shape2D(double w, double h, String k) {
+        width = w;
+        height = h;
+        kind = k;
+    }
+
+    // accessors
+    double getWidth() { return width; }
+    double getHeight() { return height; }
+    void setWidth(double w) { width = w; }
+    void setHeight(double h) { height = h; }
+    String getKind() { return kind; }
 
     void showDim() {
         System.out.println("Width and height are " + width + " and " + height);
     }
+
+    abstract double area();
 }
 
 class Triangle extends Shape2D {
-    String style;
+    private String style;
 
-    Triangle(double w, double h, String style) {
-        width = w;
-        height = h;
-        this.style = style;
+    Triangle(double w, double h, String s) {
+        // super must be the first statement executed inside a subclass constructor.
+        super(w, h, "triangle");
+
+        style = s;
     }
 
+    // override area() of Shape2D.
     double area() {
-        return width * height / 2;
+        return getWidth() * getHeight() / 2;
     }
 
     void showStyle() {
         System.out.println("Triangle is " + style);
+    }
+}
+
+class ColorTriangle extends Triangle {
+    private String color;
+
+    ColorTriangle(double w, double h, String s, String c) {
+        super(w, h, s);
+
+        color = c;
+    }
+
+    String getColor() { return color; }
+
+    void showColor() {
+        System.out.println("Color is " + color);
+    }
+}
+
+class Rectangle extends Shape2D {
+    Rectangle(double w, double h) {
+        super(w, h, "rectangle");
+    }
+
+    boolean isSquare() {
+        return getWidth() == getHeight();
+    }
+
+    double area() {
+        return getWidth() * getHeight();
     }
 }
 
@@ -42,5 +90,27 @@ class Shapes {
         t2.showStyle();
         t2.showDim();
         System.out.println("Area is " + t2.area());
+
+        System.out.println();
+
+        ColorTriangle t3 = new ColorTriangle(8.0, 12.0, "outlined", "blue");
+        System.out.println("Info for t3: ");
+        t3.showStyle();
+        t3.showDim();
+        t3.showColor();
+        System.out.println("Area is " + t3.area());
+
+        System.out.println();
+        // Abstract class
+        Shape2D[] shapes = new Shape2D[3];
+        shapes[0] = t1;
+        shapes[1] = new Rectangle(10, 5);
+        shapes[2] = t3;
+
+        for (Shape2D shape: shapes) {
+            System.out.println("object is " + shape.getKind());
+            System.out.println("area is " + shape.area());
+            System.out.println();
+        }
     }
 }
