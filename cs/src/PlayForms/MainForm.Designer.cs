73cd758c0dcf0a32cd06d44db27153073d580331
andersc <anderscui@gmail.com>
2016-01-30 00:59:12 +0800

    Parallel.ForEach

---
+++ cs/src/PlayForms/MainForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Andersc.CodePlay.PlayForms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(13, 273);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess.TabIndex = 0;
+            this.btnProcess.Text = "Process Images";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(407, 243);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(112, 272);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 326);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnProcess);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ cs/src/PlayForms/MainForm.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Andersc.CodePlay.PlayForms
+{
+    public partial class MainForm : Form
+    {
+        private CancellationTokenSource cancelToken = new CancellationTokenSource();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcess_Click(object sender, System.EventArgs e)
+        {
+            Task.Factory.StartNew(ProcessFiles);
+        }
+
+        private void ProcessFiles()
+        {
+            // Store the CancellationToken
+            var parOptions = new ParallelOptions();
+            parOptions.CancellationToken = cancelToken.Token;
+            parOptions.MaxDegreeOfParallelism = System.Environment.ProcessorCount;
+            parOptions.MaxDegreeOfParallelism = 2;
+
+            var files = Directory.GetFiles(@"C:\andersc\pics", "*.jpg", SearchOption.AllDirectories);
+            var newDir = @"C:\andersc\modified";
+            Directory.CreateDirectory(newDir);
+
+            try
+            {
+                Parallel.ForEach(files, parOptions, file =>
+                {
+                    parOptions.CancellationToken.ThrowIfCancellationRequested();
+
+                    var fName = Path.GetFileName(file);
+                    using (var bitmap = new Bitmap(file))
+                    {
+                        bitmap.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                        bitmap.Save(Path.Combine(newDir, fName));
+
+                        //this.Text = string.Format("Processing {0} on thread {1}", fName,
+                        //    Thread.CurrentThread.ManagedThreadId);
+                        this.Invoke((Action)delegate
+                        {
+                            this.Text = string.Format("Processing {0} on thread {1}", file,
+                                Thread.CurrentThread.ManagedThreadId);
+                        });
+
+                        Thread.Sleep(200);
+                    }
+                });
+
+                this.Invoke((Action)delegate
+                {
+                    this.Text = "Done!";
+                });
+            }
+            catch (OperationCanceledException ex)
+            {
+                this.Invoke((Action) delegate
+                {
+                    this.Text = ex.Message;
+                });
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            // Tell all the worker threads to stop.
+            cancelToken.Cancel();
+        }
+    }
+}
