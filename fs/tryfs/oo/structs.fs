683d3c2b94c911b4ce9389bc3bbb710989405b6b
andersc <anderscui@gmail.com>
2017-03-21 01:50:59 +0800

    add structs in fs.

---
+++ fs/tryfs/oo/structs.fs
@@ -0,0 +1,9 @@
+﻿namespace oo
+
+open System
+
+[<Struct>]
+type Circle(diameter: float) =
+  member x.getRadius() = diameter / 2.0
+  member x.Diameter = diameter
+  member x.GetArea() = Math.PI * (x.getRadius() ** 2.0)
