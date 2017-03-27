f7d111438f99b5d6da7c477c6369931be6e0d686
andersc <anderscui@gmail.com>
2017-03-27 10:39:04 +0800

    add Lark sample.

---
+++ py3/dsls/larks.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+from __future__ import absolute_import, unicode_literals
+
+import turtle
+from lark import Lark
+
+turtle_grammar = """
+    start: instruction+
+    instruction: MOVEMENT NUMBER            -> movement
+               | "c" COLOR [COLOR]          -> change_color
+               | "fill" code_block          -> fill
+               | "repeat" NUMBER code_block -> repeat
+    code_block: "{" instruction+ "}"
+    MOVEMENT: "f"|"b"|"l"|"r"
+    COLOR: LETTER+
+    %import common.LETTER
+    %import common.INT -> NUMBER
+    %import common.WS
+    %ignore WS
+"""
+
+text = """
+c red yellow
+fill { repeat 36 {
+    f200 l170
+}}
+"""
+
+parser = Lark(turtle_grammar)
+print(parser.parse(text))
+print(parser.parse(text).pretty())
