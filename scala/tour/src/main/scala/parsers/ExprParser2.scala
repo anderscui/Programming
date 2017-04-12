200ee36aac5303f0bb4adb584c69a02ca137bacf
andersc <anderscui@gmail.com>
2017-04-12 18:16:01 +0800

    add the simplest JSON Parser.

--- scala/tour/src/main/scala/parsers/ExprParser2.scala
+++ scala/tour/src/main/scala/parsers/ExprParser2.scala
@@ -1,24 +1,18 @@
 package parsers
 
-import scala.util.parsing.combinator.syntactical.StandardTokenParsers
+import scala.util.parsing.combinator.JavaTokenParsers
 
 /**
   * Created by andersc on 4/6/17.
   */
-class ExprParser2 extends StandardTokenParsers {
-  lexical.delimiters += ("+", "-", "*", "(", ")")
-
-  def expr: Parser[Any] = term ~ rep(("+" | "-") ~ term)
-  def term: Parser[Any] = factor ~ rep("*" ~> factor)
-  def factor: Parser[Any] = numericLit | "(" ~> expr <~ ")"
-
-  def parseAll[T](p: Parser[T], in: String): ParseResult[T] =
-    phrase(p)(new lexical.Scanner(in))
+class Arith extends JavaTokenParsers {
+  def expr: Parser[Any] = term ~ rep("+" ~ term | "-" ~ term)
+  def term: Parser[Any] = factor ~ rep("*" ~ factor | "/" ~ factor)
+  def factor: Parser[Any] = floatingPointNumber | "(" ~ expr ~ ")"
 }
 
-object TestExprParser2 {
+object TestArith extends Arith {
   def main(args: Array[String]): Unit = {
-    val parser = new ExprParser
-    parser.parseAll()
+    println(parseAll(expr, "2 * (3 + 7)"))
   }
 }
