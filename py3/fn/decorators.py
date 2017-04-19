d3be3e0dc3d3ea5787d258683fa25cfc47064bf8
andersc <anderscui@gmail.com>
2017-04-19 11:44:54 +0800

    add decorator in py3.

---
+++ py3/fn/decorators.py
@@ -0,0 +1,41 @@
+# coding=utf-8
+from __future__ import absolute_import, unicode_literals, division
+
+import time
+from functools import lru_cache
+
+
+def cached(func):
+    _cache = {}
+
+    def decorated(*args):
+        start_time = time.time()
+        print('_cache: ', _cache)
+        if args not in _cache:
+            _cache[args] = func(*args)
+        print('Compute time: %ss' % round(time.time() - start_time, 2))
+        return _cache[args]
+
+    return decorated
+
+
+@cached
+def complex_computation(x, y):
+    print('processing...')
+    time.sleep(1)
+    return x + y
+
+
+@lru_cache()
+def complex_computation2(x, y):
+    print('processing...')
+    time.sleep(2)
+    return x + y
+
+
+if __name__ == '__main__':
+    # print(complex_computation(1, 2))
+    # print(complex_computation(1, 2))
+
+    print(complex_computation2(1, 2))
+    print(complex_computation2(1, 2))
