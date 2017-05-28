ae3fa8353f128d6add0b5808e7531e4cac51e0da
andersc <anderscui@gmail.com>
2017-05-29 02:46:28 +0800

    step 4.

---
+++ py3/parallels/coroutines.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+def countdown(n):
+    while n > 0:
+        yield n
+        n -= 1
+
+c = countdown(5)
+print(c)
+
+# for i in c:
+#     print(i)
+
+from collections import deque
+tasks = deque()
+tasks.extend([countdown(10), countdown(5), countdown(20)])
+print(tasks)
+
+
+def run():
+    while tasks:
+        task = tasks.popleft()
+        try:
+            x = next(task)
+            print(x)
+            tasks.append(task)
+        except StopIteration:
+            print('Task done')
+
+run()
