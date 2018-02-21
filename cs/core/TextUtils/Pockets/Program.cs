112ecbfa1dd524dad34be70275e4dc0544549a2d
andersc <anderscui@gmail.com>
2018-02-22 01:59:13 +0800

    add C# 7 workbook.

---
+++ cs/core/TextUtils/Pockets/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Pockets
+{
+    public class UnitConverter
+    {
+        int ratio;
+
+        public UnitConverter(int unitRatio)
+        {
+            this.ratio = unitRatio;
+        }
+
+        public int Convert(int unit)
+        {
+            return unit * ratio;
+        }
+
+        static void Main(string[] args)
+        {
+            var feetToInches = new UnitConverter(12);
+            var milesToFeet = new UnitConverter(5280);
+
+            Console.WriteLine(feetToInches.Convert(30));
+            Console.WriteLine(feetToInches.Convert(100));
+            Console.WriteLine(feetToInches.Convert(milesToFeet.Convert(1)));
+        }
+    }
+}
