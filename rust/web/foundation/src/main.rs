f822eafd4d010263b4942e7b5004ad6b73bdfc94
andersc <anderscui@gmail.com>
2024-01-16 15:23:06 +0800

    Rust web: ch03.

--- rust/web/foundation/src/main.rs
+++ rust/web/foundation/src/main.rs
@@ -1,10 +1,13 @@
-use std::collections::HashMap;
-use std::fmt::{Display, format, Formatter};
+use std::fmt::{Display, Formatter};
 use std::io::{Error, ErrorKind};
 use std::str::FromStr;
 
-use warp::Filter;
+use warp::{Filter, filters::{cors::CorsForbidden},
+           http::Method, http::StatusCode,
+           reject::Reject, Rejection, Reply};
+use serde::Serialize;
 
+#[derive(Debug, Serialize)]
 struct QuestionId(String);
 
 impl Display for QuestionId {
@@ -24,6 +27,11 @@ impl FromStr for QuestionId {
     }
 }
 
+#[derive(Debug)]
+struct InvalidId;
+impl Reject for InvalidId {}
+
+#[derive(Debug, Serialize)]
 struct Question {
     id: QuestionId,
     title: String,
@@ -54,31 +62,59 @@ impl Display for Question {
     }
 }
 
+async fn get_questions() -> Result<impl warp::Reply, Rejection> {
+    let qid = QuestionId::from_str( "1").expect("No id provided");
+    let question = Question::new(qid,
+                                 "title".to_string(),
+                                 "content".to_string(),
+                                 Some(vec!["rust".to_string()]));
+    match question.id.0.parse::<i32>() {
+        Ok(_) => {
+            Ok(warp::reply::json(&question))
+        },
+        Err(_) => {
+            Err(warp::reject::custom(InvalidId))
+        }
+    }
+}
+
+async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
+    if let Some(error) = r.find::<CorsForbidden>() {
+        Ok(warp::reply::with_status(
+            error.to_string(),
+            StatusCode::FORBIDDEN,
+        ))
+    } else if let Some(_) = r.find::<InvalidId>() {
+        Ok(warp::reply::with_status(
+            "No valid ID presented".to_string(),
+            StatusCode::UNPROCESSABLE_ENTITY,
+        ))
+    } else {
+        Ok(warp::reply::with_status(
+            "Route not found".to_string(),
+            StatusCode::NOT_FOUND,
+        ))
+    }
+}
+
 #[tokio::main]
 async  fn main() {
-    // let qid = QuestionId::from_str( "1").expect("No id provided");
-    // let q = Question::new(qid,
-    //                       "title".to_string(),
-    //                       "content".to_string(),
-    //                       Some(vec!["rust".to_string()]));
-    // println!("{}", q);
+    let cors = warp::cors()
+        .allow_any_origin()
+        .allow_header("content-type")
+        .allow_methods(&[Method::PUT, Method::DELETE, Method::GET, Method::POST]);
 
-    let hello = warp::get()
-        .map(|| format!("Hello, world!"));
+    let get_items = warp::get()
+        .and(warp::path("questions"))
+        .and(warp::path::end())
+        .and_then(get_questions)
+        .recover(return_error);
+
+    let routes = get_items.with(cors);
 
     // curl http://localhost:3030
-    warp::serve(hello)
+    // curl http://localhost:3030/questions
+    warp::serve(routes)
         .run(([127, 0, 0, 1], 3030))
         .await;
 }
-
-// #[tokio::main]
-// async fn main() -> Result<(), Box<dyn std::error::Error>> {
-//     // get the current ip
-//     let resp = reqwest::get("https://httpbin.org/ip")
-//         .await?
-//         .json::<HashMap<String, String>>()
-//         .await?;
-//     println!("{:#?}", resp);
-//     Ok(())
-// }
